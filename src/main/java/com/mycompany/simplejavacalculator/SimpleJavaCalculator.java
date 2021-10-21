1108ebad4d735cdb4d7cb7ebbe1aa3f8a542b04f
eps <eps@192.168.98.34>
2021-10-21 18:02:54 +0200

    Línea de codigo 4 contenía fallo #10. Todo corregido

--- src/main/java/com/mycompany/simplejavacalculator/SimpleJavaCalculator.java
+++ src/main/java/com/mycompany/simplejavacalculator/SimpleJavaCalculator.java
@@ -22,11 +22,7 @@ public class SimpleJavaCalculator {
       try {
          UI uiCal = new UI();
          uiCal.init();
-         System.out.println("Codigo ejecutado");
-         System.out.println("Codigo ejecutado");
-         System.out.println("Codigo ejecutado");
-         System.out.println("Codigo ejecutado");
-         System.out.println("Codigo ejecutado");
+
       }
       catch (Exception e) {
          System.out.println(e.getMessage());   
